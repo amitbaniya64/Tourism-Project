9af9fda2e7c8f4263c7c2febe0be817d42fe8962
amitbaniya64 <amitbaniya64@gmail.com>
2017-07-15 19:56:49 -0400

    adding my project to the repository

---
+++ js/site.js
@@ -0,0 +1,75 @@
+ 
+
+/************************
+        Home page
+ ************************/
+
+         $(function() {
+             $('.pop').on('click', function(e) {
+                 e.preventDefault();
+                 $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+                 $('#imagemodal').modal('show');   
+             });     
+         });
+
+/************************
+       End Home page
+ ************************/ 
+
+
+
+
+
+/************************
+       Photos page
+ ************************/ 
+
+
+       document.getElementById('links').onclick = function (event) {
+             event = event || window.event;
+             var target = event.target || event.srcElement,
+                 link = target.src ? target.parentNode : target,
+                 options = {index: link, event: event},
+                 links = this.getElementsByTagName('a');
+             blueimp.Gallery(links, options);
+         };
+
+
+
+
+/************************
+       End Photos page
+ ************************/ 
+
+
+/************************
+       Contact page
+ ************************/ 
+
+          
+
+            function initMap() {// script for map
+              var uluru = {lat: 27.490952, lng: 83.2958898};
+                
+              var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 10,
+                center: uluru
+              });
+                
+              var marker = new google.maps.Marker({
+                position: uluru,
+                map: map
+              });
+                
+                google.maps.event.addDomListener(window, "resize", function() {
+                    var center = map.getCenter();
+                    google.maps.event.trigger(map, "resize");
+                    map.setCenter(center); 
+                });
+            }
+
+
+
+/************************
+       End Contct page
+ ************************/ 
